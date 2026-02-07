6f46b64ba29941c4112bd6504f2877c2352347e2
mayoshmase-dot <mayoshmase@gmail.com>
2026-02-07 08:23:42 +0200

    create SPA

---
+++ src/router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/login/Login";
+import Register from "./pages/auth/register/Register";
+
+const router = createBrowserRouter([
+    {
+        path : '/',
+        element : <MainLayout />,
+        children : [
+            {
+                index : true ,
+                element : <Home />
+            },
+            {
+                path : 'login',
+                element : <Login />
+            },
+            {
+                path : 'register',
+                element : <Register />
+            },
+        ] 
+    }
+]);
+export default router ;
