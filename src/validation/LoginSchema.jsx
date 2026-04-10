5f50bdd85e480bdc65501f58cb2e8d438c8931b4
mayoshmase-dot <mayoshmase@gmail.com>
2026-04-10 18:40:58 +0300

    fix error

--- src/validation/LoginSchema.jsx
+++ src/validation/LoginSchema.jsx
@@ -5,7 +5,7 @@ export const LoginSchema = yup.object({
   .email("Invalid email format").max(100, "Email is too long"),
 
   password: yup.string().required("Password is required")
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 6 characters")
     .max(20, "Password is long")
     .matches(/[A-Z]/, "Must contain at least one uppercase letter")
     .matches(/[a-z]/, "Must contain at least one lowercase letter")
